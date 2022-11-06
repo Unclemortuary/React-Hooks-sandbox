73fa20286f2486b111ff0eb49074723b236f6679
Unclemortuary <artem_mihalych@mail.ru>
2022-11-06 17:42:47 +0300

    project reorganization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import UseStateExample from './useState';
+
+const App = () => (
+  <div>
+    <h1>useState</h1>
+    <UseStateExample/>
+    <h1>useEffect</h1>
+  </div>
+)
+
+export default App;
