0dba712e837c37ff6c650d2f966429927084131b
Unclemortuary <artem_mihalych@mail.ru>
2022-11-14 19:31:56 +0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import UseStateExample from './useState';
 import UseEffectExapmle from './useEffect';
 import UseContextExample from './useContext/useContextAdvanced';
 import UseRefExample from './useRef';
+import UseMemoExample from './useMemo';
 
 const App = () => (
   <div>
-    <h1>useState</h1>
+    {/* <h1>useState</h1>
     <UseStateExample/>
     <h1>useEffect</h1>
     <UseEffectExapmle/>
     <h1>useContext</h1>
     <UseContextExample/>
     <h1>useRef</h1>
-    <UseRefExample/>
+    <UseRefExample/> */}
+    <h1>useMemo</h1>
+    <UseMemoExample />
   </div>
 );
 
