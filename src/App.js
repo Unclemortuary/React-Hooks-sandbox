b0c924db1497da2bab6a894c3b254674d7000c9f
Unclemortuary <artem_mihalych@mail.ru>
2022-11-14 23:32:20 +0300

    basic navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import UseStateExample from './useState';
 import UseEffectExapmle from './useEffect';
@@ -6,19 +6,50 @@ import UseContextExample from './useContext/useContextAdvanced';
 import UseRefExample from './useRef';
 import UseMemoExample from './useMemo';
 
-const App = () => (
-  <div>
-    {/* <h1>useState</h1>
-    <UseStateExample/>
-    <h1>useEffect</h1>
-    <UseEffectExapmle/>
-    <h1>useContext</h1>
-    <UseContextExample/>
-    <h1>useRef</h1>
-    <UseRefExample/> */}
-    <h1>useMemo</h1>
-    <UseMemoExample />
-  </div>
-);
+const topicsMap = {
+  useState: UseStateExample,
+  useEffect: UseEffectExapmle,
+  useContext: UseContextExample,
+  useRef: UseRefExample,
+  useMemo: UseMemoExample
+};
+
+const App = () => {
+  const [ current, setCurrent ] = useState(null);
+
+  const onTopicClick = (topicName) => setCurrent(topicName);
+
+  const renderTopic = () => {
+    const TopicComponent = topicsMap[current];
+    return <TopicComponent/>
+  };
+  
+  return (
+    <div>
+      { !current && <Navigation onTopicClick={onTopicClick}></Navigation>}
+      { current && 
+      <div className='contentContainer'>
+        <button className='backButton' onClick={() => setCurrent(null)}>Back</button>
+        <section>
+          { renderTopic() }
+        </section>
+      </div> }
+    </div>
+  );
+};
+
+const Navigation = ({ onTopicClick }) => {
+  const names = Object.keys(topicsMap);
+
+  const topicClickHandler = (e) => onTopicClick(e.target.innerText);
+
+  return (
+    <nav>
+      <ul className='navigation'>
+        { names.map((name => <li key={name}><button onClick={topicClickHandler}>{name}</button></li>)) }
+      </ul>
+    </nav>
+  )
+}
 
 export default App;
