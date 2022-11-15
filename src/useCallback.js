c19f5b2ff82778a2323a08f41991fd6e6832cdaa
Unclemortuary <artem_mihalych@mail.ru>
2022-11-15 13:16:04 +0300

    Made sure that event listenres should not be wrapped into useCallback

--- src/useCallback.js
+++ src/useCallback.js
@@ -6,6 +6,7 @@ import React, { useState, useCallback, useEffect } from "react";
 const UseCallbackExample = () => {
     const [ number, setNumber ] = useState(0);
     const [ dark, setDark ] = useState(false);
+    const [ showSlider, setShowSlider ] = useState(true);
 
     // Most common use case - getNumbers recreated only when "number" changed
     // so that child component won't rerender when any other props in parent component changes
@@ -25,6 +26,11 @@ const UseCallbackExample = () => {
             <div style={themeStyles}>
                 <ListDependantOnParentsFunction getNumbers={getNumbers}/>
             </div>
+            <button 
+                onClick={() => setShowSlider(previousValue => !previousValue)}>
+                { showSlider ? 'Hide' : 'Show' } Slider
+            </button>
+            { showSlider ? <SliderWithEventListener/> : null }
         </>
     );
 };
@@ -37,6 +43,39 @@ const ListDependantOnParentsFunction = ({ getNumbers }) => {
     return <ul>
         {getNumbers().map(number => <li key={number.toString()}>{number}</li>)}
     </ul>
+};
+
+
+// Made sure that event listenres should not be wrapped into useCallback
+const SliderWithEventListener = () => {
+    const [ sliderNumber, setSliderNumber ] = useState(0);
+
+    const handleWindowResize = () => {
+        console.log(window.innerWidth);
+    };
+
+    useEffect(() => {
+        console.log('new function created');
+    }, [handleWindowResize]);
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        }
+    });
+
+    return (
+        <div>
+            <input 
+                type='range' 
+                min='0' 
+                max='50'
+                onChange={(e) => setSliderNumber(parseInt(e.target.value, 10))}></input>
+            <span>{sliderNumber}</span>
+        </div>
+    );
 };
 
 export default UseCallbackExample;
